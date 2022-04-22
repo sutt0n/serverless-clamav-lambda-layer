b71ef4f54b42ddadf2ffc753f08ab74d7ce0cb5c
Joseph Sutton <ebwebdev@gmail.com>
2022-04-21 19:45:15 -0500

    fixed lambda image build script and permission problems

--- build.sh
+++ build.sh
@@ -4,12 +4,3 @@ rm -rf ./layer
 mkdir layer
 
 docker build -t clamav -f Dockerfile .
-docker run --name clamav clamav
-docker cp clamav:/home/build/clamav_lambda_layer.zip .
-docker rm clamav
-mv clamav_lambda_layer.zip ./layer
-
-pushd layer
-unzip -n clamav_lambda_layer.zip
-rm clamav_lambda_layer.zip
-popd
--- handler.js
+++ handler.js
@@ -15,21 +15,31 @@ module.exports.virusScan = async (event, context) => {
       console.log("Not an S3 Record!");
       continue;
     }
+    
+    const key = decodeURIComponent(record.s3.object.key.replace(/\+g/, " "));
+
+    console.log('getting the file', key, record);
 
     // get the file
     const s3Object = await s3
       .getObject({
         Bucket: record.s3.bucket.name,
-        Key: record.s3.object.key
+        Key: record.s3.object.key,
       })
       .promise();
+      
+    console.log('got the file', record.s3.object.key);
 
     // write file to disk
     writeFileSync(`/tmp/${record.s3.object.key}`, s3Object.Body);
+
+    console.log('wrote the file');
     
-    try { 
+    try {
       // scan it
       execSync(`./bin/clamscan --database=./var/lib/clamav /tmp/${record.s3.object.key}`);
+
+      console.log('updating tag to clean');
 
       await s3
         .putObjectTagging({
@@ -45,7 +55,10 @@ module.exports.virusScan = async (event, context) => {
           }
         })
         .promise();
+
+      console.log('updated tag to clean');
     } catch(err) {
+      console.log('err', err);
       if (err.status === 1) {
         // tag as dirty, OR you can delete it
         await s3
